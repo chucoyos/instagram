05bf23c0bffa7aff0a77c8430a12190db943810a
chucoyos <facturasmanzanillo@hotmail.com>
2021-01-21 23:35:12 -0600

    add profile update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :users, only: [:show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+  
 end
